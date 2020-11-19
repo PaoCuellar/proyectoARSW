78293c5eceaac8f7c6d616594b750af06368df32
diegofch29 <diegofch29@hotmail.com>
2020-11-19 02:40:37 -0500

    real time fixed

--- src/main/resources/static/js/apiclient.js
+++ src/main/resources/static/js/apiclient.js
@@ -2,8 +2,7 @@ api = (function () {
 
     return {
          getSubastaById: function(subasta_id, callback){
-             //$.getJSON("https://subastasdinipa.herokuapp.com/subastas/subasta/" + subasta_id, function (data) {
-             $.getJSON("http://localhost:8080/subastas/subasta/" + subasta_id, function (data) {
+             $.getJSON("https://subastasdinipa.herokuapp.com/subastas/subasta/" + subasta_id, function (data) {
                 callback(data);
             });
          },
@@ -11,7 +10,7 @@ api = (function () {
          postSubastaPush: function(subasta_id,user_id,push) {
             $.ajax({
                 type: "POST",
-                url: "http://localhost:8080/subastas/subasta/push", //"https://subastasdinipa.herokuapp.com/subastas/subasta/push",
+                url: "https://subastasdinipa.herokuapp.com/subastas/subasta/push",
                 data: JSON.stringify({
                     "subasta_id": subasta_id,
                     "user_id": user_id,
