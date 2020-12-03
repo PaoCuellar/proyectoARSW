ce83073796d94f4a6fa076382cc3971b36f9efcf
PaoCuellar <paola.cuellar@mail.escuelaing.edu.co>
2020-12-03 00:24:32 -0500

    listas administrador

--- src/main/resources/static/js/administrator.js
+++ src/main/resources/static/js/administrator.js
@@ -22,43 +22,43 @@ $(document).ready(function() {
 
     $.get("https://subastasdinipa.herokuapp.com/subastas/usuarios", function(data, status){
         console.log("Data: ",data, "\nStatus: ", status);
-        var items = "<tr>\n" +
-                    "<th>Producto</th>\n" +
-                    "<th>Precio</th>\n" +
-                    "<th>Vendido</th>\n" +
+        var users = "<tr>\n" +
+                    "<th>Usuario</th>\n" +
+                    "<th>Nombre</th>\n" +
+                    "<th>Correo</th>\n" +
+                    "<th>Ciudad</th>\n" +
                     "</tr>";
 
 
-        for (const item of data){
+        for (const user of data){
 
-            items +="<tr>\n" +
-                    "<th>" + item.description + "</th>\n" +
-                    "<th>" + item.hopedPrice + "</th>\n" +
-                    "<th>" + item.sold + "</th>\n" +
+            users +="<tr>\n" +
+                    "<th>" + user.userName + "</th>\n" +
+                    "<th>" + user.name + "</th>\n" +
+                    "<th>" + user.email + "</th>\n" +
+                    "<th>" + user.ciudad + "</th>\n" +
                     "</tr>"
         }
-        document.getElementById('ItemTable').innerHTML =items;
+        document.getElementById('UserTable').innerHTML =users;
     });
 
 
     $.get("https://subastasdinipa.herokuapp.com/subastas/categorias", function(data, status){
         console.log("Data: ",data, "\nStatus: ", status);
-        var items = "<tr>\n" +
-                    "<th>Producto</th>\n" +
-                    "<th>Precio</th>\n" +
-                    "<th>Vendido</th>\n" +
-                    "</tr>";
+        var categorys = "<tr>\n" +
+                        "<th>Categoría</th>\n" +
+                        "<th>Descripción</th>\n" +
+                        "</tr>";
 
 
-        for (const item of data){
+        for (const category of data){
 
-            items +="<tr>\n" +
-                    "<th>" + item.description + "</th>\n" +
-                    "<th>" + item.hopedPrice + "</th>\n" +
-                    "<th>" + item.sold + "</th>\n" +
-                    "</tr>"
+            categorys +="<tr>\n" +
+                        "<th>" + category.category + "</th>\n" +
+                        "<th>" + category.description + "</th>\n" +
+                        "</tr>"
         }
-        document.getElementById('ItemTable').innerHTML =items;
+        document.getElementById('CategoryTable').innerHTML =categorys;
     });
 })
 
